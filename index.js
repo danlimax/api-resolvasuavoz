bcb232d2fbbad490564452aaeda435210042a829
Daniel Lima <daniellimaa05@gmail.com>
2023-11-21 18:43:25 -0300

    Arquivo env adicionado

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 const cors = require("cors");
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "admin",
-  password: "admin",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "resolvasuavoz",
 });
 const app = express();
